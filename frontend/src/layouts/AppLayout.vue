0dd46af3ba8a5d1ae9e3f7ca36f74747022d16b2
Andrey Pechennikov <ap@devandy.me>
2023-12-21 14:04:57 +0600

    Add routes

--- frontend/src/layouts/AppLayout.vue
+++ frontend/src/layouts/AppLayout.vue
@@ -1,10 +1,38 @@
 <script setup>
-import AppHeader from "@/layouts/AppHeader.vue";
+import { shallowRef, watch } from "vue";
+import { useRoute } from "vue-router";
+import AppLayoutDefault from "@/layouts/DefaultLayout.vue";
+
+const route = useRoute();
+const layout = shallowRef(null);
+
+// Наблюдаем за изменением маршрута
+watch(
+  () => route.meta,
+  async (meta) => {
+    try {
+      if (meta.layout) {
+        // Пробуем найти компонент из свойства meta и динамически импортировать его
+        const component = await import(`./${meta.layout}.vue`);
+        layout.value = component?.default || AppLayoutDefault;
+      } else {
+        layout.value = AppLayoutDefault;
+      }
+    } catch (e) {
+      console.error(
+        "Динамический шаблон не найден. Установлен шаблон по-умолчанию.",
+        e,
+      );
+      layout.value = AppLayoutDefault;
+    }
+  },
+);
 </script>
 
 <template>
-  <app-header />
-  <slot />
+  <component :is="layout">
+    <slot />
+  </component>
 </template>
 
 <style scoped lang="scss"></style>
---
+++ frontend/src/layouts/DefaultLayout.vue
@@ -0,0 +1,19 @@
+<script setup>
+import DefaultHeader from "@/layouts/DefaultHeader.vue";
+</script>
+
+<template>
+  <div class="default-layout">
+    <default-header />
+    <slot />
+  </div>
+</template>
+
+<style scoped lang="scss">
+.default-layout {
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  flex: 1;
+}
+</style>
